c456377463380dca76227577db4b78a29050a0ea
J <jordimodernel@gmail.com>
2023-08-22 21:49:34 -0300

    Actividad Complementaria 7

--- cypress/e2e/actividadComplementaria7.cy.js
+++ cypress/e2e/actividadComplementaria7.cy.js
@@ -1,5 +1,15 @@
+import { ToDoListPage } from "../support/pages/toDoListPage";
+
 describe('Actividad complementaria 7', () => { 
-    
+    let datos;
+    const toDoListPage = new ToDoListPage();
+
+before('Before', () => {
+    cy.fixture('datos').then(data => {
+    datos = data;
+})
+})
+
     beforeEach("Actividad complementaria 7", () => {
        cy.visit('');
        cy.get("#registertoggle").dblclick()
@@ -11,14 +21,21 @@ describe('Actividad complementaria 7', () => {
    })
 
      it("Deberia sacar una foto completa", () =>{
-       //agregar 3 tareas y sacar una foto a la pantalla completa
-   })
- 
-   it("Deberia sacar una foto a las tres tareas", () =>{
-       //agregar 3 tareas y sacar una foto a las tareas (las 3 tareas en la misma foto - cuadro celeste)
-   })
-})
+        cy.screenshot({capture:"fullPage"});
+        //agregar 3 tareas y sacar una foto a la pantalla completa
+   });
 
+it("Deberia sacar una foto a las tres tareas", () =>{
+    //agregar 3 tareas y sacar una foto a las tareas (las 3 tareas en la misma foto - cuadro celeste)
+    toDoListPage.escribirTarea(datos.primerTest.tarea1)
+    toDoListPage.clickSendtask();
+    toDoListPage.escribirTarea(datos.primerTest.tarea2)
+    toDoListPage.clickSendtask();
+    toDoListPage.escribirTarea(datos.primerTest.tarea3)
+    toDoListPage.clickSendtask();
+    cy.get('.css-ha1fhc').screenshot("Tareas")
+})
+})
 // Instalar git
 // Inicializa un repositorio en github desde visual studio code
 // Subi el repositorio completo. Recorda usar .gitignore para ignorar la carpeta node modules
